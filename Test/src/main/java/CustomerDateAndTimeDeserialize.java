c6b6fa1b7e6ff627f65782e6b00a6665e1c49d84
elenaandries <ramonabastic@gmail.com>
2018-06-26 00:25:49 +0300

    Update CustomerDateAndTimeDeserialize.java

--- Test/src/main/java/CustomerDateAndTimeDeserialize.java
+++ Test/src/main/java/CustomerDateAndTimeDeserialize.java
@@ -29,3 +29,56 @@ public class CustomerDateAndTimeDeserialize extends JsonDeserializer<Date> {
         return paramDeserializationContext.parseDate(str);
     }
 }
+
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class ConfigImplTest {
+    @Test
+    public void shouldNotFail_whenLoadCorrectFile() {
+        Config config = new ConfigImpl("test.properties");
+
+        try {
+            config.load();
+        } catch (Exception e) {
+            fail("Should not fail");
+        }
+    }
+
+    @Test(expected = Exception.class)
+    public void shouldFail_whenLoadWrongFile() throws Exception {
+        Config config = new ConfigImpl("file-does-not-exists.properties");
+        config.load();
+    }
+
+    @Test
+    public void shouldGetCorrectValue_whenLoadFile() {
+        Config config = new ConfigImpl("test.properties");
+        try {
+            config.load();
+        } catch (Exception e) {
+            fail("Should not fail");
+        }
+
+        assertTrue(config.getProperty("test.key").equals("value"));
+    }
+}
+
+in test/resource/test.properties
+test.key=value
+
+public class UnmarshallTest {
+
+//    @Test
+//    public void unmarshal() {
+//        try (InputStream path = this.getClass().getClassLoader().getResourceAsStream("good.xml")) {
+//            Files.copy(InputStream path, Path target);
+//        } catch (IOException e) {
+//            throw e;
+//        }
+//    }
+} sa iei din jar
