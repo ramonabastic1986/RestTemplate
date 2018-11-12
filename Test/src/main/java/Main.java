30a9b28b9e3eba873a2d1e8a4bf2741756702268
elenaandries <ramonabastic@gmail.com>
2018-11-12 23:26:15 +0200

    Update Main.java

--- Test/src/main/java/Main.java
+++ Test/src/main/java/Main.java
@@ -90,5 +90,38 @@ public class Main {
 
         return href3;
     }
+    
+    private int compareVersions(String version1, String version2) {
+    if (version1 == null || version2 == null) {
+        throw new IllegalArgumentException("Versions can not be null")
+    }
+
+    def arr1 = version1.split("\\.")
+    def arr2 = version2.split("\\.")
+
+    int i = 0
+
+    while (i < arr1.length || i < arr2.length) {
+        if (i < arr1.length && i < arr2.length) {
+            if (Integer.parseInt(arr1[i]) < Integer.parseInt(arr2[i])) {
+                return -1
+            } else if (Integer.parseInt(arr1[i]) > Integer.parseInt(arr2[i])) {
+                return 1
+            }
+        } else if (i < arr1.length) {
+            if (Integer.parseInt(arr1[i]) != 0) {
+                return 1
+            }
+        } else if (i < arr2.length) {
+            if (Integer.parseInt(arr2[i]) != 0) {
+                return -1
+            }
+        }
+
+        i++
+    }
+
+    return 0
+}
 }
 
